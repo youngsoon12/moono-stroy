e620499e92d538f4bb41a3526a1c1a5714b74b9a
youngsoon12 <enejwk789@gmail.com>
2024-09-06 17:46:45 +0900

    add:img 테스트, 회원가입API구현

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "../pages/Main";
+import Test from 'pages/Test';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
