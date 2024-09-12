15ee2bab8159b2fc9e42da27cceaffef6d3f447f
youngsoon12 <enejwk789@gmail.com>
2024-09-12 17:45:11 +0900

    z

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -5,6 +5,7 @@ import LoginPage from 'pages/LoginPage';
 import ProtectedRoute from './ProtectedRoute';
 import RegisterPage from 'pages/RegisterPage';
 import Main from 'pages/Main';
+import PhotoPage from 'pages/PhotoPage';
 
 const Router = () => {
   return (
@@ -14,7 +15,7 @@ const Router = () => {
         <Route path="/main" element={<Main />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-
+        <Route path="/photo" element={<PhotoPage />} />
         <Route element={<ProtectedRoute />}>
           {/* 로그인이 필요한 라우트 같은 경우에는 아래에 작성. */}
         </Route>
---
+++ src/pages/PhotoPage.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PhotoPage = () => {
+  return <div></div>;
+};    
+
+export default PhotoPage;
