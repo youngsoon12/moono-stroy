927b0354cef91617b2bbc033a98d1d9318c97fff
sieon <sieon.eve@gmail.com>
2024-09-16 15:31:43 +0900

    Set Maincolor in the theme

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Container from '../components/css/Container';
+import theme from 'styles/theme';
 
 const Main = (props: any) => {
   return (
@@ -17,15 +18,30 @@ const Main = (props: any) => {
     >
       {/* 첫 번째 섹션 */}
       <GradientDiv>
-        <div style={{ width: '100%', height: '7vh', zIndex: '2', display: 'grid',
-  gridTemplateColumns: 'repeat(4, 1fr)', alignContent:'center', border:"1px solid"}}>
-        
-          <div style={{gridColumn:'2',textAlign:'start'}}>제목</div>
+        <MainHeaderStyle
+          style={{
+            width: '100%',
+            height: '7vh',
+            zIndex: '2',
+            display: 'grid',
+            gridTemplateColumns: 'repeat(4, 1fr)',
+            alignContent: 'center',
+            border: '1px solid',
+          }}
+        >
+          <div style={{ gridColumn: '2', textAlign: 'start' }}>제목</div>
           <div>도장</div>
           <div>이름</div>
-        </div>
-        <img src={`${process.env.PUBLIC_URL}/images/main/HiMoono.png`} 
-        style={{ position: 'absolute', height: '38vh', zIndex: '2', top: '4vh' }} />
+        </MainHeaderStyle>
+        <img
+          src={`${process.env.PUBLIC_URL}/images/main/HiMoono.png`}
+          style={{
+            position: 'absolute',
+            height: '38vh',
+            zIndex: '2',
+            top: '4vh',
+          }}
+        />
         <IntroText>
           <span>안녕?</span>
           <span>내 이야기를 들어줄래?</span>
@@ -34,20 +50,18 @@ const Main = (props: any) => {
 
       {/* 두 번째 섹션 */}
       <MenuContainer>
+        <ButtonContainer>
+          {/* 상단 2개의 버튼 */}
+          <MenuButton>무너소개서</MenuButton>
+          <MenuButton>무 퀴즈~?!</MenuButton>
 
-      < ButtonContainer>
-        {/* 상단 2개의 버튼 */}
-        <MenuButton>무너소개서</MenuButton>
-        <MenuButton>무 퀴즈~?!</MenuButton>
+          {/* 가운데 큰 버튼 */}
+          <LargeButton>무너를 응원해줘!</LargeButton>
 
-        {/* 가운데 큰 버튼 */}
-        <LargeButton>무너를 응원해줘!</LargeButton>
-
-        {/* 하단 2개의 버튼 */}
-        <MenuButton>무너네컷</MenuButton>
-        <MenuButton>오늘의 운세</MenuButton>
-
-      </ ButtonContainer>
+          {/* 하단 2개의 버튼 */}
+          <MenuButton>무너네컷</MenuButton>
+          <MenuButton>오늘의 운세</MenuButton>
+        </ButtonContainer>
       </MenuContainer>
     </Container>
   );
@@ -63,7 +77,7 @@ const GradientDiv = styled.div`
   flex-direction: column;
   align-items: center;
   position: relative;
-  margin-bottom: 5dvh ; 
+  margin-bottom: 5dvh;
 
   &::before {
     content: '';
@@ -72,7 +86,7 @@ const GradientDiv = styled.div`
     left: 0;
     width: 100%;
     height: 26vh;
-    background-color: #E947AE; /* 상단 70% 분홍색 */
+    background-color: ${theme.color.mainColor}; /* 상단 70% 분홍색 */
     z-index: 1; /* 상단 배경 요소의 z-index를 낮게 설정 */
   }
 `;
@@ -80,8 +94,8 @@ const MenuContainer = styled.div`
   width: 100%;
   height: 58vh;
   display: flex;
-  flex-direction: column;`
-;
+  flex-direction: column;
+`;
 const IntroText = styled.div`
   position: absolute;
   z-index: 2;
@@ -95,7 +109,7 @@ const IntroText = styled.div`
 `;
 
 const ButtonContainer = styled.div`
- display: grid;
+  display: grid;
   grid-template-columns: repeat(2, 1fr); /* 2열로 구성 */
   grid-gap: 2vh;
   width: 100%;
@@ -104,7 +118,7 @@ const ButtonContainer = styled.div`
 `;
 
 const MenuButton = styled.button`
-background-color: #fff;
+  background-color: #fff;
   border-radius: 10px;
   height: 12vh;
   font-size: 1.2em;
@@ -113,16 +127,30 @@ background-color: #fff;
   align-items: center;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.329);
   font-weight: bold;
-  &&:hover{
+  &&:hover {
     box-shadow: 0 4px 6px rgba(0, 0, 0, 0.562);
   }
 `;
 
 const LargeButton = styled(MenuButton)`
   grid-column: span 2; /* 2칸을 차지하도록 설정 */
-  background-color: #E947AE; /* 핑크색 배경 */
+  background-color: ${theme.color.mainColor}; /* 핑크색 배경 */
   color: #fff; /* 글자색 흰색 */
   font-weight: bold;
   height: 14vh;
   font-size: 1.5em;
+`;
+
+const MainHeaderStyle = styled.div`
+  position: absolute;
+  top: 0;
+  display: flex;
+  /* align-items: center; */
+  /* width: 100%; */
+  /* min-height: 10dvh; */
+  /* max-width: 480px; 모바일에서 최대 너비 고정 */
+  /* padding: 20px 30px; */
+  box-sizing: border-box;
+  /* border: 1px solid #333333; */
+  overflow: auto;
 `;
--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,3 +1,4 @@
+// GlobalStyle.ts
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 const GlobalStyle = createGlobalStyle`
@@ -12,121 +13,29 @@ const GlobalStyle = createGlobalStyle`
         url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Thin.woff') format('woff'),
         url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Thin.ttf') format("truetype");
     font-display: swap;
-}
-@font-face {
-    font-family: 'Pretendard';
-    font-weight: 200;
-    font-style: normal;
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.eot');
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.eot?#iefix') format('embedded-opentype'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.woff2') format('woff2'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.woff') format('woff'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraLight.ttf') format("truetype");
-    font-display: swap;
-}
-@font-face {
-    font-family: 'Pretendard';
-    font-weight: 300;
-    font-style: normal;
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Light.eot');
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Light.eot?#iefix') format('embedded-opentype'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Light.woff2') format('woff2'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Light.woff') format('woff'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Light.ttf') format("truetype");
-    font-display: swap;
-}
-@font-face {
-    font-family: 'Pretendard';
-    font-weight: 400;
-    font-style: normal;
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.eot');
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.eot?#iefix') format('embedded-opentype'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.woff2') format('woff2'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.woff') format('woff'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Regular.ttf') format("truetype");
-    font-display: swap;
-}
-@font-face {
-    font-family: 'Pretendard';
-    font-weight: 500;
-    font-style: normal;
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Medium.eot');
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Medium.eot?#iefix') format('embedded-opentype'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Medium.woff2') format('woff2'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Medium.woff') format('woff'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Medium.ttf') format("truetype");
-    font-display: swap;
-}
-@font-face {
-    font-family: 'Pretendard';
-    font-weight: 600;
-    font-style: normal;
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.eot');
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.eot?#iefix') format('embedded-opentype'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.woff2') format('woff2'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.woff') format('woff'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-SemiBold.ttf') format("truetype");
-    font-display: swap;
-}
-@font-face {
-    font-family: 'Pretendard';
-    font-weight: 700;
-    font-style: normal;
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Bold.eot');
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Bold.eot?#iefix') format('embedded-opentype'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Bold.woff2') format('woff2'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Bold.woff') format('woff'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Bold.ttf') format("truetype");
-    font-display: swap;
-}
-@font-face {
-    font-family: 'Pretendard';
-    font-weight: 800;
-    font-style: normal;
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraBold.eot');
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraBold.eot?#iefix') format('embedded-opentype'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraBold.woff2') format('woff2'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraBold.woff') format('woff'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-ExtraBold.ttf') format("truetype");
-    font-display: swap;
-}
-@font-face {
-    font-family: 'Pretendard';
-    font-weight: 900;
-    font-style: normal;
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Black.eot');
-    src: url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Black.eot?#iefix') format('embedded-opentype'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Black.woff2') format('woff2'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Black.woff') format('woff'),
-        url('https://cdn.jsdelivr.net/gh/webfontworld/pretendard/Pretendard-Black.ttf') format("truetype");
-    font-display: swap;
-}
-@font-face {
-    font-family: 'Cafe24Ssurround';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/Cafe24Ssurround.woff') format('woff');
-    font-weight: normal;
-    font-style: normal;
-}
-   /* 전역 기본 스타일 설정 */
+  }
+  /* 다른 @font-face 정의 생략 */
+  
   body {
     margin: 0;
     padding: 0;
-    font-family: 'Pretendard', sans-serif; /* 적용한 폰트 사용 */
+    font-family: 'Pretendard', sans-serif;
     box-sizing: border-box;
     font-weight: 400;
     background-color: #f0f0f0;
-  *:focus{
+  }
+
+  *:focus {
     outline: none;
   }
-  button{
+
+  button {
     border: none;
     cursor: pointer;
-    color: black;
-    :active{
-        text-decoration: none;
-        color: black;
+    &:active {
+      text-decoration: none;
     }
   }
-}
 `;
+
 export default GlobalStyle;
