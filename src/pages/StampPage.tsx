31897118850166d8a8dc42b0b3c2cf2c27fedefb
sieon <sieon.eve@gmail.com>
2024-09-25 17:23:37 +0900

    feat: 화면 비율조정 진행중

--- src/pages/StampPage.tsx
+++ src/pages/StampPage.tsx
@@ -19,7 +19,6 @@ export const StampPage = (props: any) => {
     { id: 3, title: '무너 응원', completed: true },
     { id: 4, title: '오늘 운세', completed: true },
     { id: 5, title: '무너 네컷', completed: true },
-
   ]);
 
   useEffect(() => {
@@ -42,59 +41,60 @@ export const StampPage = (props: any) => {
   };
 
   return (
-    <Container
-      style={{
-        background:
-          'linear-gradient(180deg, rgba(255,255,255,1) 24%, rgba(243,224,136,1) 100%)',
-        height: '100%',
-      }}
-    >
+    <div>
       <Header>{'미션진행도'}</Header>
-      <CoffeImgSection>
-        <CoffeText>
-          <div>
-            <span style={{ color: `${theme.color.mainColor}` }}>무스</span>
-            구경하고
-          </div>
-          <div>아메리카노 마시자!</div>
-        </CoffeText>
-        <img
-          src={`${process.env.PUBLIC_URL}/images/stamp/coffeimga.png`}
-          style={{ width: '80%', height: '80%', margin: '0 auto' }}
-        />
-      </CoffeImgSection>
-      <StampSection>
-        <StampText>
-          <div style={{ fontSize: '1.2em' }}>
-            미션을 완료하고 도장을 모아보세요.
-          </div>
-          <div style={{ fontSize: '0.8em', marginTop: '10px' }}>
-            도장을 다 모으면 응모 가능해요!
-          </div>
-        </StampText>
-        <StampArea>
-          <StampRow>
-            {stamps.slice(0, 3).map((stamp) => (
-              <Stamp
-                key={stamp.id}
-                page={stamp.title}
-                completed={stamp.completed}
-              />
-            ))}
-          </StampRow>
-          <StampRow>
-            {stamps.slice(3, 5).map((stamp) => (
-              <Stamp
-                key={stamp.id}
-                page={stamp.title}
-                completed={stamp.completed}
-              />
-            ))}
-          </StampRow>
-        </StampArea>
-        <ApplyButton onClick={() => completeMission(1)}>응모하기</ApplyButton>
-      </StampSection>
-    </Container>
+      <Container
+        style={{
+          background:
+            'linear-gradient(180deg, rgba(255,255,255,1) 24%, rgba(243,224,136,1) 100%)',
+        }}
+      >
+        <CoffeImgSection>
+          <CoffeText>
+            <div>
+              <span style={{ color: `${theme.color.mainColor}` }}>무스</span>
+              구경하고
+            </div>
+            <div>아메리카노 마시자!</div>
+          </CoffeText>
+          <img
+            src={`${process.env.PUBLIC_URL}/images/stamp/coffeimga.png`}
+            style={{ width: '80%', height: '80%', margin: '0 auto' }}
+          />
+        </CoffeImgSection>
+        <StampSection>
+          <StampText>
+            <div style={{ fontSize: '1.2em' }}>
+              미션을 완료하고 도장을 모아보세요.
+            </div>
+            <div style={{ fontSize: '0.8em', marginTop: '10px' }}>
+              도장을 다 모으면 응모 가능해요!
+            </div>
+          </StampText>
+          <StampArea>
+            <StampRow>
+              {stamps.slice(0, 3).map((stamp) => (
+                <Stamp
+                  key={stamp.id}
+                  page={stamp.title}
+                  completed={stamp.completed}
+                />
+              ))}
+            </StampRow>
+            <StampRow>
+              {stamps.slice(3, 5).map((stamp) => (
+                <Stamp
+                  key={stamp.id}
+                  page={stamp.title}
+                  completed={stamp.completed}
+                />
+              ))}
+            </StampRow>
+          </StampArea>
+        </StampSection>
+      </Container>
+      <ApplyButton onClick={() => completeMission(1)}>응모하기</ApplyButton>
+    </div>
   );
 };
 
@@ -104,7 +104,7 @@ const CoffeImgSection = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  margin: 10dvh auto 0;
+  /* margin: 0 auto 0; */
 `;
 
 const CoffeText = styled.div`
@@ -143,11 +143,14 @@ const StampRow = styled.div`
 `;
 
 const ApplyButton = styled.button`
-  width: 100%;
-  height: 80px;
-  align-items: flex-end;
+  width: 480px;
+  height: 30%;
+  /* align-items: flex-end; */
   justify-content: center;
   background-color: ${theme.color.mainColor};
   color: #fff;
   font-size: 1.4em;
+  left: 50%;
+  margin: 0 auto;
+  /* transform: translateX(-50%); 자신의 너비의 절반만큼 왼쪽으로 이동 */
 `;
