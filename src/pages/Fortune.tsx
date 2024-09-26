c5a913e8e1f6769070cd4981d6b4135613440a0b
youngsoon12 <enejwk789@gmail.com>
2024-09-26 22:59:24 +0900

    oneMission, fiveMission stamp 연동완

--- src/pages/Fortune.tsx
+++ src/pages/Fortune.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import theme from 'styles/theme';
 import Container from '../components/css/Container';
 import Header from '../components/form/Header';
-import Stamp from '@components/css/Stamp';
+import Stamp from '../components/css/Stamp';
 export const Fortune = (props: any) => {
   return (
     <div>
